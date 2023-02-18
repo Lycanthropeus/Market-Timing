c75af5901c37e3ac9ed7fa39bc09270da430759c
Niranjan <niranjan.r@holosuit.com>
2023-02-18 14:13:52 +0530

    Data imports

--- VAR.R
+++ VAR.R
@@ -1,4 +1,11 @@
 suppressMessages(suppressWarnings({
 library(dplyr)
 library(vars)
+library(lubridate)
 }))
+
+
+cfnai_u = read.csv("./data/cfnai.csv")
+cfnai_u$Date = as.Date(parse_date_time(cfnai_u$Date,"dmy"))
+tibble(cfnai_u)
+
