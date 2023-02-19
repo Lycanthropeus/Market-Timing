233c1c31ffb356c0ec31f1f77ae6dd53c6e6e5b6
Niranjan <niranjan.r@holosuit.com>
2023-02-19 09:29:53 +0530

    Updated data files

--- VAR.R
+++ VAR.R
@@ -2,10 +2,14 @@ suppressMessages(suppressWarnings({
 library(dplyr)
 library(vars)
 library(lubridate)
+library(ggplot2)
 }))
 
 
 cfnai_u = read.csv("./data/cfnai.csv")
 cfnai_u$Date = as.Date(parse_date_time(cfnai_u$Date,"dmy"))
-tibble(cfnai_u)
+cfnai_u = tibble(cfnai_u);cfnai_u
+
+ggplot(data = cfnai_u,mapping=aes(x=Date,y=SP500.Values)) +
+  geom_point()+geom_line()
 
