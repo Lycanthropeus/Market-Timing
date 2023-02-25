e252d92fe236fd88fa0b179dd3d7fce15edd74eb
Lycanthropeus <niranjanrao@iisc.ac.in>
2023-02-25 11:47:00 +0530

    Working version of expanding window

--- DecisionTree.R
+++ DecisionTree.R
@@ -1,10 +1,11 @@
+source("./helper_functions.R")
+cfnai = load_and_preprocess_data()
+
+
 library(tree)
-library(PerformanceAnalytics)
 
 # Todo @ Niranjan 
 # First implement expanding window decision tree.
-source("./helper_functions.R")
-cfnai = load_and_preprocess_data()
 
 prices = cfnai$SP500.Values
 
@@ -21,14 +22,60 @@ df$returns_ts = Return.calculate(df$prices_ts,"discrete")
 
 training_set = df["1984-12-31::1995"] 
 
-model = tree(returns_ts ~ cfnaima3_ts + diffusion_ts,data=df)
+model = tree(returns_ts ~ cfnaima3_ts + diffusion_ts,data=training_set)
 plot(model)
 text(model,pretty=1)
 
-predict(model,df["1996-01-31"])
+predicted_init = predict(model,df["1996-01-31"])
 df["1996-01-31"]
 
-plot(df$returns_ts)
+predicted_init$
 # training_set_expand = df["1984-12-31::1996-31-01"]
-View(df$prices_ts)
-plot(df$prices_ts)
+# View(df$prices_ts)
+# plot(df$prices_ts)
+
+
+data.list <- split(df,'days')
+
+# walkforward_df = list("Date"=,"Predicted_Returns"=3,"Actual_Returns"=5)
+# names(walkforward_df) = c("Date","Predicted_Returns","Actual_Returns")
+walkforward_df = data.frame("Predicted_Returns"=3.5,"Actual_Returns"=5.5)
+walkforward_df
+
+
+lis = index(df)
+for(idx in seq_along(lis)){
+  x = as.Date(lis[[idx]])
+  if(x >= "1995-12-31" & x<"2022-12-31"){
+    training_data = df[paste0("1984-01-31::",x)]
+    model = tree(returns_ts ~ diffusion_ts + cfnaima3_ts,data = training_data)
+    loc_prediction = predict(model,df[lis[[idx+1]]])
+    
+    g = data.frame(as.numeric(df[lis[[idx+1]]]$returns_ts),as.numeric(predict(model,df[lis[[idx+1]]])))
+    names(g) = c("Predicted_Returns","Actual_Returns")
+    
+    walkforward_df = rbind(walkforward_df,g)
+  }
+}
+walkforward_df
+# as.xts(walkforward_df,order.by = )
+# i = 1
+# for(date in names(data.list)){
+#   if(i < 458) {
+#     if(!date <= "1996-01-31"){
+#       training_data = df[paste0("1984-12-31::",date)]
+#       model = tree(returns_ts ~ cfnaima3_ts + diffusion_ts,data = training_data)
+#       predicted_next = predict(model,df[i+1,])
+#       
+#       print(predicted_next)
+#       # c = df[i+1,]$returns_ts
+#       # next_date = as.Date(index(df[i+1,])) 
+#       # loc_list = list("Date" = next_date,"Predicted_Returns" = predicted_next,"Actual_Returns" = c)
+#       # # walkforward_df = append(walkforward_df, loc_list)
+#       # print(loc_list)
+#       # walkforward_df %>% add_row()
+#       # walkforward_df = rbind.data.frame(walkforward_df,loc_list)
+#     }
+#     i = i+1
+#   }
+# }
--- helper_functions.R
+++ helper_functions.R
@@ -1,9 +1,21 @@
-
 load_and_preprocess_data = function() {
   setwd("D:/4th Sem Work/Research Work_Rahul/Market-Timing/")
   .libPaths("D:/4th Sem Work/Research Work_Rahul/Market-Timing/libs/")
+  suppressMessages(suppressWarnings({
+    library(dplyr)
+    library(vars)
+    library(lubridate)
+    library(ggplot2)
+    library(tseries)
+    library(xts)
+    library(PerformanceAnalytics)
+  }))
   cfnai_u = read.csv("./data/cfnai.csv")
   cfnai_u$Date = as.Date(parse_date_time(cfnai_u$Date,"dmy"))
   cfnai_u = tibble(cfnai_u);cfnai_u
   return (cfnai_u)
+}
+
+expanding_window_error = function(sdate){
+  
 }
